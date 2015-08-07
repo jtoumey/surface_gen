aff86c6f1d948935b8303fa229c288b0de8e0fd0
jtoumey <jtoumey@gmail.com>
2015-08-06 23:17:52 -0500

    initial header definition

---
+++ surface_gen.f90
@@ -0,0 +1,21 @@
+!*************************************************************************!
+!                                                                         !
+!  File:         surface_gen.f90                                          !
+!                                                                         !
+!  Programmer:   Julian M. Toumey                                         !
+!                Madison, WI                                              !
+!                                                                         !
+!  Date:         August 2015                                              !
+!                                                                         !
+!  Language:     FORTRAN 90                                               !
+!                                                                         !
+!  Description:  This module generates an *.stl surface file based on     !
+!                user inputs.                                             !
+!                                                                         !
+!  Output Files: surface  Output file containing surface coordinates      !
+!                                                                         !
+!*************************************************************************!
+PROGRAM SURFACE_GEN
+IMPLICIT NONE
+!
+
