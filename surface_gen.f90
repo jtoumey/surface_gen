0a1a9f67c97e0d3f713667ee2e341d73a8aed00b
jtoumey <jtoumey@gmail.com>
2015-08-06 23:52:00 -0500

    added read file code for a file of known length

--- surface_gen.f90
+++ surface_gen.f90
@@ -18,9 +18,11 @@
 PROGRAM SURFACE_GEN
 IMPLICIT NONE
 !
+integer n,ii
+parameter(n=5)
+!
 character(len=25) :: file_name
-
-
+double precision x(n),y(n)
 !
 !...INPUT SECTION
 !
@@ -31,4 +33,21 @@ read(*,*)file_name
 write(*,*)'YOU ENTERED: '
 write(*,*)file_name
 
+!
+!...
+!
+open(unit=2,file = file_name)
+write(*,*)'READING INPUT FILE...'
+!...loop over lines in file
+!
+do ii = 1,n
+   read(2,*)x(ii),y(ii)
+   write(*,101)ii,x(ii),y(ii)
+end do
+close(2)
+!
+write(*,*)'...FINISHED READING FILE.'
+!
+101 format(3x,'*** n = ',i4,3x,'x = ',f14.7,3x,'y = ',f14.7,' ***')
+!
 END 
