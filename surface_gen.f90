89a26005ef23e17180f6470ad9e8f3ea7c94a1b0
jtoumey <jtoumey@gmail.com>
2015-08-07 20:56:00 -0500

    added code to parse the number of points in the first line of the file and read in the corresponding number of points

--- surface_gen.f90
+++ surface_gen.f90
@@ -18,8 +18,11 @@
 PROGRAM SURFACE_GEN
 IMPLICIT NONE
 !
-integer n,ii
+integer n,ii,iblnk
 parameter(n=5)
+
+character(len=1000) :: buffer
+integer num_points
 !
 character(len=25) :: file_name
 double precision x(n),y(n)
@@ -29,23 +32,42 @@ double precision x(n),y(n)
 write(*,*)'ENTER INPUT FILE NAME: '
 read(*,*)file_name
 !
-write(*,*)'YOU ENTERED: '
+write(*,*)'FILE NAME YOU ENTERED: '
 write(*,*)file_name
 !
 !...
 !
 open(unit=2,file = file_name)
 write(*,*)'READING INPUT FILE...'
+!
+read(2,'(a)')buffer
+print*,buffer(1:10)
+!
+if (buffer(1:10) == 'num_points') then
+   !read(buffer(11:1000),*)num_points
+   iblnk = index(buffer,'	')
+   read(buffer(iblnk:),*)num_points
+   !
+   print *,num_points
+
+else
+   print *, '!!! FIRST LINE OF FILE MUST CONTAIN KEYWORD num_points, TAB CHARACTER, THEN NUMBER OF POINTS. !!!'
+   stop
+endif
+!
+
+
+
 !...loop over lines in file
 !
-do ii = 1,n
+do ii = 1,num_points
    read(2,*)x(ii),y(ii)
    write(*,101)ii,x(ii),y(ii)
 end do
 close(2)
 !
 write(*,*)'...FINISHED READING FILE.'
-write(*,202)n
+write(*,202)num_points
 !
 101 format(3x,'*** n = ',i4,3x,'x = ',f14.7,3x,'y = ',f14.7,' ***')
 202 format(3x,'READ',i4,3x,'DATA POINTS.')
