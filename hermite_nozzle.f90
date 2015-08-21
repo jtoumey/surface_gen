db8b6f366f02d82a4e4d763ce5c8c7e67a8c76d3
jtoumey <jtoumey@gmail.com>
2015-08-20 23:28:36 -0500

    added test for hermite nozzle function

--- hermite_nozzle.f90
+++ hermite_nozzle.f90
@@ -1,37 +1,11 @@
-!*************************************************************************!
-!                                                                         !
-!  Subroutine:   HERMITE_NOZZLE.f90                                       !
-!                                                                         !
-!  Programmer:   Julian M. Toumey                                         !
-!                Madison, WI                                              !
-!                                                                         !
-!  Date:         August 2015                                              !
-!                                                                         !
-!  Language:     FORTRAN 90                                               !
-!                                                                         !
-!  Description:  This subroutine creates a 2D geometry for a converging-  !
-!                diverging nozzle using Hermite interpolation between     !
-!                specific points in a C-D nozzle.                         !
-!                                                                         !
-!                Inputs:                                                  !
-!                   get_input   control for input loop in main routine    !
-!                                                                         !
-!  Output Files: surface  Output file containing surface coordinates      !
-!                                                                         !
-!*************************************************************************!
 SUBROUTINE HERMITE_NOZZLE(get_input)
-IMPLICIT NONE
-!
-logical get_input
-!
-write(*,*)'A______             ______'
-write(*,*)'        \          /'
-write(*,*)'          \______/'
-write(*,*)''
 
-!
-!...Set input switch, so main routine will exit input loop
-!
+logical get_input
+
+
+write(*,*)'HERMITE NOZZLE TEST'
+
 get_input = .TRUE.
-!
+
+
 END SUBROUTINE HERMITE_NOZZLE
