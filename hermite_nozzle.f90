d2e8000277ef3978b2afe9e3b82a5452488c0550
Julian T <jtoumey@gmail.com>
2015-08-23 17:27:21 -0500

    work on hermite interpolation

--- hermite_nozzle.f90
+++ hermite_nozzle.f90
@@ -1,9 +1,18 @@
 SUBROUTINE HERMITE_NOZZLE(get_input)
 
+
+
 logical get_input
 
+double precision a,b,c,d,e,a1,a2
 
-write(*,*)'HERMITE NOZZLE TEST'
+
+write(*,*)'ENTER A,B,C,D,E,A1,A2: '
+read(*,*)a,b,c,d,e,a1,a2
+
+
+
+!write(*,*)'HERMITE NOZZLE TEST'
 
 get_input = .TRUE.
 
