176e2adc2709967387dc761ea0d535c7ef46407b
jtoumey <jtoumey@gmail.com>
2017-09-24 18:25:38 -0500

    interpolate

---
+++ interpolate_divided_differences.f90
@@ -0,0 +1,14 @@
+program interpolate_divided_difference
+implicit none
+!
+integer n,ii
+double precision, dimension(3) :: x,y
+!
+n = 4
+x = (/ 1., 2., 3. /)
+y = (/ 2., 3., 6. /)
+!
+
+
+!
+end program interpolate_divided_difference
